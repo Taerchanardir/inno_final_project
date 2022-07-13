25fb3468806e9357eb677a33e0717a624fe4af14
User <User@MyComputer>
2022-07-13 13:21:53 +0100

    Assignment coursework 1st version

---
+++ README.md
@@ -0,0 +1,12 @@
+file has to be in markdown
+
+just the links in markdown
+
+portfolio url, on github,  = notes from lessons
+'backend project' url, on github, the 1st small project, within the portfolio project
+this weeks long project url on github
+kanban board url
+
+
+link to deployed/hosted project
+
--- views.py
+++ views.py
@@ -9,21 +9,22 @@ def index():
 def page1():
   return render_template("page1.html")
 
-@my_view.route('/page2')
-def page2():
-  return render_template("page2.html")
 
-@my_view.route('/page3')
-def page3():
-  return render_template("page3.html")
+# @my_view.route('/page2')
+# def page2():
+#   return render_template("page2.html")
 
-@my_view.route('/page4')
-def page4():
-  return render_template("page4.html")
+# @my_view.route('/page3')
+# def page3():
+#   return render_template("page3.html")
 
-@my_view.route('/page5')
-def page5():
-  return render_template("page5.html")
+# @my_view.route('/page4')
+# def page4():
+#   return render_template("page4.html")
+
+# @my_view.route('/page5')
+# def page5():
+#   return render_template("page5.html")
 
 @my_view.route('/js')
 @my_view.route('/javascript')
@@ -31,9 +32,9 @@ def page5():
 def general_redirects():
   return redirect(url_for("my_view.index"))
 
-@my_view.route('/admin')
-def admin_page():
-  return render_template("admin.html")
+# @my_view.route('/admin')
+# def admin_page():
+#   return render_template("admin.html")
 
 
 
